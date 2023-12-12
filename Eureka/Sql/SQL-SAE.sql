0145b50a21b32dc505f761f9b286da5870ad9a3c
Chaipo-arch <81694579+Chaipo-arch@users.noreply.github.com>
2023-12-12 09:39:25 +0100

    création de toutes les tables qui sont dans le shéma BD relationel

---
+++ Eureka/Sql/SQL-SAE.sql
@@ -0,0 +1,171 @@
+CREATE DATABASE Eureka;
+use Eureka;
+
+
+-- Création de toutes les tables
+Drop TABLE if EXISTs filiere;
+CREATE TABLE filiere(
+    
+    id int(15) AUTO_INCREMENT,
+    year int(1),
+    field Varchar(25),
+    course char(1),
+    PRIMARY KEY(id)
+    );
+
+
+Drop TABLE if EXISTs role;
+CREATE Table role(
+    id int(15) AUTO_INCREMENT,
+    designation Varchar(25),
+    PRIMARY key(id)
+);
+
+Drop Table if EXISTs Utilisateur;
+CREATE Table Utilisateur(
+    id int(15) AUTO_INCREMENT,
+    Username Varchar(30),
+    nom Varchar(40),
+    prenom Varchar(25),
+    password TEXT,
+    id_role int(15),
+    id_filiere int(15),
+    FOREIGN KEY (id_role) REFERENCES role(id),
+    FOREIGN KEY (id_filiere) REFERENCES filiere(id),
+    PRIMARY Key(id) 
+);
+
+Drop Table if EXISTs Entreprise;
+CREATE Table Entreprise(
+    id int(15) AUTO_INCREMENT ,
+    Designation Varchar(25),
+    activity_sector Varchar(25),
+    logo TEXT,
+    presentation Text,
+    PRIMARY Key(id)
+);
+
+Drop Table if EXISTs souhaitEntreprise;
+CREATE Table souhaitEntreprise(
+    
+    DesignationDuPoste Varchar(50),
+    id_entreprise int(15),
+    id_filiere int(15),
+    FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id),
+    FOREIGN KEY (id_filiere) REFERENCES filiere(id),
+    PRIMARY Key(id_entreprise,id_filiere) 
+);
+
+Drop Table if EXISTs souhaitEtudiant;
+CREATE Table souhaitEtudiant(
+    
+    id_entreprise int(15),
+    id_utilisateur int(15),
+    
+    FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id),
+    FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id),
+    PRIMARY Key(id_entreprise,id_utilisateur) 
+);
+
+Drop Table if EXISTs RDV;
+CREATE Table RDV(
+    id int(15) AUTO_INCREMENT,
+    Heure_debut Time,
+    Heure_fin Time,
+    dateRDV date,
+    id_souhait int(30),
+
+    id_souhait_entreprise INT(15),  -- Utiliser une colonne différente pour la référence
+    id_souhait_utilisateur INT(15),  -- Utiliser une colonne différente pour la référence
+    FOREIGN KEY (id_souhait_entreprise, id_souhait_utilisateur) REFERENCES souhaitEtudiant(id_entreprise, id_utilisateur),
+    PRIMARY KEY (id)
+);
+
+Drop Table if EXISTs Lieu;
+CREATE Table Lieu(
+    id int(15) AUTO_INCREMENT,
+    ville Varchar(25),
+    cp int(5),
+    adresse  Varchar(50),
+    num_rue int(5),
+    id_entreprise int(15),
+   
+    FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id),
+    PRIMARY KEY (id)
+    
+ 
+);
+
+Drop Table if EXISTs Intervenants;
+CREATE Table Intervenants(
+    id int(15) AUTO_INCREMENT,
+    name Varchar(25),
+    id_entreprise int(15),
+
+    FOREIGN KEY (id_entreprise) REFERENCES Entreprise(id),
+    PRIMARY KEY (id)
+    
+ 
+);
+
+
+Drop Table if EXISTs Forum;
+CREATE Table Forum(
+    id int(15) AUTO_INCREMENT,
+    Heure_debut Time,
+    Heure_fin Time,
+    duree Time,
+    date_limite dateTime,
+    PRIMARY KEY (id)
+    
+ 
+);
+-- Fin de la création des table
+
+-- Procedure de connexion en cours de réalisation
+DROP PROCEDURE IF EXISTS connexion;
+
+DELIMITER //
+CREATE PROCEDURE connexion(
+    IN p_nom_utilisateur VARCHAR(25),
+    IN p_mot_de_passe TEXT
+)
+BEGIN
+    DECLARE compte_existant INT;
+    START TRANSACTION;   
+    SELECT COUNT(*) INTO compte_existant
+    FROM utilisateurs
+    WHERE nom_utilisateur = p_nom_utilisateur AND mot_de_passe = p_mot_de_passe;
+
+    IF compte_existant < 1 THEN 
+        SELECT FALSE AS est_utilisateur;
+    ELSE 
+        SELECT TRUE AS est_utilisateur;
+    END IF;
+    COMMIT;
+END//
+DELIMITER ;
+
+INSERT INTO role Value()
+DROP PROCEDURE IF EXISTS CreateAdmin;
+
+--DELIMITER //
+--CREATE PROCEDURE CreateAdmin(
+  --  IN username VARCHAR(30),
+    --IN p_mot_de_passe TEXT
+--)
+--BEGIN
+  --  DECLARE compte_existant INT;
+    --START TRANSACTION;   
+    --SELECT COUNT(*) INTO compte_existant
+    --FROM utilisateurs
+    --WHERE nom_utilisateur = p_nom_utilisateur AND mot_de_passe = p_mot_de_passe;
+
+    --IF compte_existant < 1 THEN 
+    --    SELECT FALSE AS est_utilisateur;
+    --ELSE 
+      --  SELECT TRUE AS est_utilisateur;
+    --END IF;
+    --COMMIT;
+--END//
+--DELIMITER ;
