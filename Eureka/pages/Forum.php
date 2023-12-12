518ff344f1ecef145164654e03368d9872a1f17a
ALEXANDRE BROUZES <alexandre.brouzes@iut.local>
2023-12-12 09:36:31 +0100

    correction arborescence

--- Eureka/pages/Forum.php
+++ Eureka/pages/Forum.php
@@ -1,4 +1,3 @@
-
 <html>
 	<head>
 		<title>Forum</title>
---
+++ Eureka/pages/pageInformationEntreprise.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Eureka</title>
+	<meta charset ="utf-8"/>
+	<link href="../bootstrap-4.6.2-dist/css/bootstrap.css" rel="stylesheet"/>
+	<link href="../css/Style.css" rel="stylesheet"/>
+	<link href="../fontawesome-free-6.2.1-web/css/all.css" rel="stylesheet"/>
+</head>	
+<body>
+       
+</body>
+</html>
